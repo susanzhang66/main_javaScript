7a0d31bae757208b70258b7b31070fde6f2f98a6
“usanzhang666 <1347059561@qq.com>
2018-08-03 17:52:58 +0800

    del pa

--- vue/my-project/mockData/iloan/bank/alter.do
+++
@@ -1,8 +0,0 @@
-{
-    "flag": "1",
-    "msg": "成功",
-    "data": {
-        "resultCode": "1",
-        "resultMsg": "返回结果信息"
-    }
-}
--- vue/my-project/src/router/signature.js
+++
@@ -1,8 +0,0 @@
-// webpack 在编译时，会静态地解析代码中的 require.ensure()，同时将模块添加到一个分开的 chunk 当中。这个新的 chunk 会被 webpack 通过 jsonp 来按需加载。
-const signature = r => require.ensure([], () => r(require('@/components/signature')), 'group-signature')
-
-export default {
-  path: '/signature',
-  name: 'signature',
-  component: signature
-}
